c7fc25fec278cad1c5f5bb505b118e0519cfcac4
Aymeric PICARD MARCHETTO <aymericpm@gmail.com>
2018-02-09 13:20:42 +0100

    Changed to GSon since it's apparently better

--- src/com/nullprogram/chess/resources/JSONLoader.java
+++ src/com/nullprogram/chess/resources/JSONLoader.java
@@ -2,6 +2,7 @@ package com.nullprogram.chess.resources;
 
 public class JSONLoader {
 	private static final String PATH = "./resources/pieces/";
+	
 	private JSONLoader() {}
 	
 	public void loadPieces() {
