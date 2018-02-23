60887536409b8189ebca7258531e4cd9130966f1
Lykrast <aymericpm@gmail.com>
2018-02-23 23:21:29 +0100

    More minor cleanup

--- src/com/nullprogram/chess/boards/StandardBoard.java
+++ src/com/nullprogram/chess/boards/StandardBoard.java
@@ -2,10 +2,8 @@ package com.nullprogram.chess.boards;
 
 import com.nullprogram.chess.Board;
 import com.nullprogram.chess.GameMode;
-import com.nullprogram.chess.Model;
 import com.nullprogram.chess.Piece;
 import com.nullprogram.chess.Position;
-import com.nullprogram.chess.pieces.PieceRegistry;
 
 /**
  * The board for a standard game of chess.
@@ -15,79 +13,11 @@ public class StandardBoard extends Board {
     /** Serialization identifier. */
     private static final long serialVersionUID = -484123716L;
 
-    /** The standard board width. */
-    static final int WIDTH = 8;
-
-    /** The standard board height. */
-    static final int HEIGHT = 8;
-
-    /** Row of the white pawns. */
-    static final int WHITE_PAWN_ROW = 1;
-
-    /** Row of the black pawns. */
-    static final int BLACK_PAWN_ROW = 6;
-
-    /** White home row. */
-    static final int WHITE_ROW = 0;
-
-    /** Black home row. */
-    static final int BLACK_ROW = 7;
-
-    /** Queen side rook column. */
-    static final int Q_ROOK = 0;
-
-    /** Queen side knight column. */
-    static final int Q_KNIGHT = 1;
-
-    /** Queen side bishop column. */
-    static final int Q_BISHOP = 2;
-
-    /** Queen column. */
-    static final int QUEEN = 3;
-
-    /** King column. */
-    static final int KING = 4;
-
-    /** King side bishop column. */
-    static final int K_BISHOP = 5;
-
-    /** King side knight column. */
-    static final int K_KNIGHT = 6;
-
-    /** King side rook column. */
-    static final int K_ROOK = 7;
-
     /**
      * The standard chess board.
      */
     public StandardBoard() {
-    	super(new GameMode("FIDE", 8,8));
-        setWidth(WIDTH);
-        setHeight(HEIGHT);
-        clear();
-        Model pawn = PieceRegistry.get("pawn"), knight = PieceRegistry.get("knight"), rook = PieceRegistry.get("rook"),
-        		bishop = PieceRegistry.get("bishop"), queen = PieceRegistry.get("queen"), king = PieceRegistry.get("king");
-        
-        for (int x = 0; x < WIDTH; x++) {
-            setPiece(x, WHITE_PAWN_ROW, new Piece(Piece.Side.WHITE, pawn));
-            setPiece(x, BLACK_PAWN_ROW, new Piece(Piece.Side.BLACK, pawn));
-        }
-        setPiece(Q_ROOK, WHITE_ROW, new Piece(Piece.Side.WHITE, rook));
-        setPiece(K_ROOK, WHITE_ROW, new Piece(Piece.Side.WHITE, rook));
-        setPiece(Q_ROOK, BLACK_ROW, new Piece(Piece.Side.BLACK, rook));
-        setPiece(K_ROOK, BLACK_ROW, new Piece(Piece.Side.BLACK, rook));
-        setPiece(Q_KNIGHT, WHITE_ROW, new Piece(Piece.Side.WHITE, knight));
-        setPiece(K_KNIGHT, WHITE_ROW, new Piece(Piece.Side.WHITE, knight));
-        setPiece(Q_KNIGHT, BLACK_ROW, new Piece(Piece.Side.BLACK, knight));
-        setPiece(K_KNIGHT, BLACK_ROW, new Piece(Piece.Side.BLACK, knight));
-        setPiece(Q_BISHOP, WHITE_ROW, new Piece(Piece.Side.WHITE, bishop));
-        setPiece(K_BISHOP, WHITE_ROW, new Piece(Piece.Side.WHITE, bishop));
-        setPiece(Q_BISHOP, BLACK_ROW, new Piece(Piece.Side.BLACK, bishop));
-        setPiece(K_BISHOP, BLACK_ROW, new Piece(Piece.Side.BLACK, bishop));
-        setPiece(QUEEN, WHITE_ROW, new Piece(Piece.Side.WHITE, queen));
-        setPiece(QUEEN, BLACK_ROW, new Piece(Piece.Side.BLACK, queen));
-        setPiece(KING, WHITE_ROW, new Piece(Piece.Side.WHITE, king));
-        setPiece(KING, BLACK_ROW, new Piece(Piece.Side.BLACK, king));
+    	super(GameModeRegistry.get("fide"));
     }
     
     public StandardBoard(GameMode mode)
