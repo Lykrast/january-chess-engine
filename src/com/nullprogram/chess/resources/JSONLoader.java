e04738cabc0e8988945c3d5860b91f03bccb278e
Lykrast <aymericpm@gmail.com>
2019-11-10 00:01:08 +0100

    Added error messages when pieces and/or gamemodes fail to load

--- src/com/nullprogram/chess/resources/JSONLoader.java
+++ src/com/nullprogram/chess/resources/JSONLoader.java
@@ -5,6 +5,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.logging.Logger;
 
+import javax.swing.JOptionPane;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonParseException;
@@ -21,12 +23,16 @@ import com.nullprogram.chess.pieces.movement.MoveType;
 public class JSONLoader {
 	private static final String PATH_PIECES = "./resources/pieces";
 	private static final String PATH_GAMEMODES = "./resources/gamemodes";
+	private static final int PATH_LENGTH_PIECES = PATH_PIECES.length() + 1;
+	private static final int PATH_LENGTH_GAMEMODES = PATH_GAMEMODES.length() + 1;
 
     /** This class's Logger. */
-    private static final Logger LOG =
-        Logger.getLogger("com.nullprogram.chess.resources.JSONLoader");
+    private static final Logger LOG = Logger.getLogger("com.nullprogram.chess.resources.JSONLoader");
 	
 	private JSONLoader() {}
+	
+	private static int errorCount;
+	private static StringBuilder errorBuilder;
 	
 	public static void loadPieces() {
 		MoveType.registerDeserializers();
@@ -36,18 +42,21 @@ public class JSONLoader {
 				.create();
 		
 		File folder = new File(PATH_PIECES);
+		errorCount = 0;
+		errorBuilder = new StringBuilder();
 		loadPiecesFolder(g, folder);
+		if (errorCount > 0) {
+			errorBuilder.insert(0, errorCount + " pieces could not be loaded.");
+			JOptionPane.showMessageDialog(null, errorBuilder.toString(), "Piece loading error", JOptionPane.ERROR_MESSAGE);
+		}
 	}
 	
-	private static void loadPiecesFolder(Gson g, File folder)
-	{
+	private static void loadPiecesFolder(Gson g, File folder) {
 		File[] pieces = folder.listFiles();
-		
-		for (File f : pieces)
-		{
+
+		for (File f : pieces) {
 			if (f.isDirectory()) loadPiecesFolder(g, f);
-			else if (f.getName().endsWith(".json"))
-			{
+			else if (f.getName().endsWith(".json")) {
 				try {
 					try (FileReader reader = new FileReader(f)) {
 						Model m = g.fromJson(reader, Model.class);
@@ -56,12 +65,19 @@ public class JSONLoader {
 						PieceRegistry.register(name, m);
 					}
 					catch (JsonParseException e) {
-			            String message = "Failed to parse piece: " + f + ": " + e;
-			            LOG.severe(message);
+						String message = "Failed to parse piece " + f.toString().substring(PATH_LENGTH_PIECES) + ": " + e;
+						LOG.severe(message);
+						errorCount++;
+						errorBuilder.append(System.lineSeparator());
+						errorBuilder.append(message);
 					}
-				} catch (IOException e) {
-		            String message = "Failed to load piece file: " + f + ": " + e;
-		            LOG.severe(message);
+				}
+				catch (IOException e) {
+					String message = "Failed to load piece file " + f.toString().substring(PATH_LENGTH_PIECES) + ": " + e;
+					LOG.severe(message);
+					errorCount++;
+					errorBuilder.append(System.lineSeparator());
+					errorBuilder.append(message);
 				}
 			}
 		}
@@ -77,18 +93,22 @@ public class JSONLoader {
 				.create();
 		
 		File folder = new File(PATH_GAMEMODES);
-		loadGameModesFolder(g, folder);		
+		errorCount = 0;
+		errorBuilder = new StringBuilder();
+		loadGameModesFolder(g, folder);
+		if (errorCount > 0) {
+			errorBuilder.insert(0, errorCount + " gamemodes could not be loaded.");
+			JOptionPane.showMessageDialog(null, errorBuilder.toString(), "Gamemode loading error", JOptionPane.ERROR_MESSAGE);
+		}
+		errorBuilder = null;
 	}
 	
-	private static void loadGameModesFolder(Gson g, File folder)
-	{
+	private static void loadGameModesFolder(Gson g, File folder) {
 		File[] pieces = folder.listFiles();
-		
-		for (File f : pieces)
-		{
+
+		for (File f : pieces) {
 			if (f.isDirectory()) loadGameModesFolder(g, f);
-			if (f.getName().endsWith(".json"))
-			{
+			if (f.getName().endsWith(".json")) {
 				try {
 					try (FileReader reader = new FileReader(f)) {
 						GameMode m = g.fromJson(reader, GameMode.class);
@@ -97,14 +117,21 @@ public class JSONLoader {
 						GameModeRegistry.register(name, m);
 					}
 					catch (JsonParseException e) {
-			            String message = "Failed to parse gamemode: " + f + ": " + e;
-			            LOG.severe(message);
+						String message = "Failed to parse gamemode " + f.toString().substring(PATH_LENGTH_GAMEMODES) + ": " + e;
+						LOG.severe(message);
+						errorCount++;
+						errorBuilder.append(System.lineSeparator());
+						errorBuilder.append(message);
 					}
-				} catch (IOException e) {
-		            String message = "Failed to load gamemode file: " + f + ": " + e;
-		            LOG.severe(message);
+				}
+				catch (IOException e) {
+					String message = "Failed to load gamemode file " + f.toString().substring(PATH_LENGTH_GAMEMODES) + ": " + e;
+					LOG.severe(message);
+					errorCount++;
+					errorBuilder.append(System.lineSeparator());
+					errorBuilder.append(message);
 				}
 			}
-		}	
+		}
 	}
 }
