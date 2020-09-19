d373c69006b58949fe9d160d2363d97bbd066640
Lykrast <aymericpm@gmail.com>
2020-09-19 02:00:19 +0200

    Added chess.com color theme

--- src/com/nullprogram/chess/gui/ColorTheme.java
+++ src/com/nullprogram/chess/gui/ColorTheme.java
@@ -30,12 +30,13 @@ public class ColorTheme {
 	static {
 		themes = new ArrayList<>();
 		themes.add(new ColorTheme("Wood", 0xD18B47, 0xFFCE9E));
+		themes.add(new ColorTheme("Lichess", 0xB58863, 0xF0D9B5));
+		themes.add(new ColorTheme("Chess.com", 0x769656, 0xEEEED2));
 		themes.add(new ColorTheme("Metal", 0xC4C4C4, 0xE7E7E7));
 		themes.add(new ColorTheme("Garden", 0x339933, 0xCCCC11));
 		themes.add(new ColorTheme("Metro", 0x527362, 0xF1F2CB));
 		themes.add(new ColorTheme("Ancient", 0x8F976D, 0xF2F2B8));
 		themes.add(new ColorTheme("Lava", 0x605E5F, 0xA0676E));
-		themes.add(new ColorTheme("Lichess", 0xB58863, 0xF0D9B5));
 		themes.add(new ColorTheme("Cream", 0xEBAC00, 0xEBEBEB));
 		themes.add(new ColorTheme("Olive", 0x808000, 0xF0F0F0));
 		themes.add(new ColorTheme("Blue", 0x4B4BA4, 0xF0F0F0));
